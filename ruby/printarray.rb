c38d07368f57ed1c207e19436d5a7ff0a3db7651
tylooor <tylooor@tamu.edu>
2020-04-06 01:53:50 +0000

    added ruby code from slides, printarray

---
+++ ruby/printarray.rb
@@ -0,0 +1,16 @@
+# Program to print elements of an array
+# Save this as printarray.rb
+
+array1 = ["Ruby", "Perl", "C++", "Java", "Python"]
+puts "Unix can be programmed in all these languages: "
+puts " "
+for array_element in array1
+          puts array_element
+end
+TIME = [Time.now]
+puts " "
+puts "The current time is: "
+puts TIME
+puts " "
+
+
